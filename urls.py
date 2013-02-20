e11f42dbbf79a268c57d3be9cade2ad82d361820
Steve Pomeroy <spomeroy@mit.edu>
2013-02-20 15:54:37 -0500

    Adds serving of static files when in debug mode

--- urls.py
+++ urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import settings
 
 urlpatterns = patterns('',
     url(r'^', include('traveler.urls')),
 )
 
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+             url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+               {'document_root': settings.MEDIA_ROOT}))
