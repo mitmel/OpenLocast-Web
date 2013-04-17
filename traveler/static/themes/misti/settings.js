d45ed5f9dff6f7fadc69cee1406cb65bdeb63a4a
Nicholas Wallen <nicholas.a.wallen@gmail.com>
2013-04-17 16:40:32 -0400

    Adds theme for MISTI

---
+++ traveler/static/themes/misti/settings.js
@@ -0,0 +1,18 @@
+// settings.js
+// use this file to pass theme-specific setting to the Openlocast JavaScript application
+
+
+// initialize or add to existing locast object 
+
+var locast = locast || {};
+
+// map cluster style "image" or "circles"
+
+locast.MAP_CLUSTER_STYLE = "image";
+
+// terminology
+
+locast.CAST_NAME = "Cast";
+locast.CAST_NAME_PLURAL = "Casts";
+locast.COLLECTION_NAME = "Collection";
+locast.COLLECTION_NAME_PLURAL = "Collections";
